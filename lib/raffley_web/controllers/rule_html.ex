07cc4157d58a0adf59154eabc265365597b28140
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-11-17 01:02:42 +0300

    add the rules/:id (show) - inline template

--- lib/raffley_web/controllers/rule_html.ex
+++ lib/raffley_web/controllers/rule_html.ex
@@ -2,4 +2,13 @@ defmodule RaffleyWeb.RuleHTML do
   use RaffleyWeb, :html
 
   embed_templates "rule_html/*"
+
+  def show(assigns) do
+    ~H"""
+    <div class="rules">
+      <h1><%= @greeting %>, don't forget...</h1>
+      <p><%= @rule.text %></p>
+    </div>
+    """
+  end
 end
