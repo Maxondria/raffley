2798362c582e1739e0c0b8be352d2e19339085b8
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-11-19 14:30:25 +0300

    function components bro - phoenix is awesome!

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -2,8 +2,46 @@ defmodule RaffleyWeb.RaffleLive.Index do
   use RaffleyWeb, :live_view
 
   alias Raffley.Raffles
+  alias Raffley.Raffle
 
   def mount(_params, _session, socket) do
     {:ok, socket |> assign(raffles: Raffles.list())}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div class="raffle-index">
+      <div class="raffles">
+        <.raffle_card :for={raffle <- @raffles} raffle={raffle} />
+      </div>
+    </div>
+    """
+  end
+
+  attr :status, :atom, required: true, values: [:upcoming, :open, :closed]
+
+  def badge(assigns) do
+    ~H"""
+    <div class="badge">
+      <%= @status %>
+    </div>
+    """
+  end
+
+  attr :raffle, Raffle, required: true
+
+  def raffle_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@raffle.image_path} alt={@raffle.description} />
+      <h2><%= @raffle.prize %></h2>
+      <div class="details">
+        <div class="price">
+          <%= @raffle.ticket_price %> / ticket
+        </div>
+        <.badge status={@raffle.status} />
+      </div>
+    </div>
+    """
   end
 end
