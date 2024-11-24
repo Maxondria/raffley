00c1c23547b1e089b912cb6b0784c072f37b6f66
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-11-24 23:32:40 +0300

    to ecto

--- lib/raffley/raffles.ex
+++ lib/raffley/raffles.ex
@@ -1,44 +1,13 @@
 defmodule Raffley.Raffles do
   alias Raffley.Raffles.Raffle
+  alias Raffley.Repo
 
   def list do
-    [
-      %Raffle{
-        id: 1,
-        prize: "Autographed Jersey",
-        ticket_price: 2,
-        status: :open,
-        image_path: "/images/jersey.jpg",
-        description: "Step up sports fans!"
-      },
-      %Raffle{
-        id: 2,
-        prize: "Coffe With a Yeti",
-        ticket_price: 3,
-        status: :upcoming,
-        image_path: "/images/yeti-coffee.jpg",
-        description: "A super-chill coffee date."
-      },
-      %Raffle{
-        id: 3,
-        prize: "A Yeti Cooler",
-        ticket_price: 1,
-        status: :closed,
-        image_path: "/images/comic-book.jpg",
-        description: "A rare collectible!"
-      }
-    ]
+    Repo.all(Raffle)
   end
 
-  def get_one!(id) when is_integer(id) do
-    list()
-    |> Enum.find(&(&1.id == id))
-  end
-
-  def get_one!(id) when is_binary(id) do
-    id
-    |> String.to_integer()
-    |> get_one!()
+  def get_one!(id) do
+    Repo.get!(Raffle, id)
   end
 
   def get_featured(raffle) do
